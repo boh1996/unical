1e15cad4f1df757e50a96fd35e102aad56f346ad
Frederik Lassen <frederiklassen@gmail.com>
2014-10-24 22:54:48 +0200

    Cleanup and testing

---
+++ api/controllers/LectioController.js
@@ -0,0 +1,15 @@
+/**
+ * LectioController
+ *
+ * @description :: Server-side logic for managing lectios
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	timetable: function (req, res) {
+		// [ branch: '517', section: 'asdasd', year: '2014' ]
+		//return res.json();
+		return res.send("Test")
+	}
+};
+
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,8 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  'get /lectio/:branch/:section/:year': 'LectioController.timetable'
 
   /***************************************************************************
   *                                                                          *
