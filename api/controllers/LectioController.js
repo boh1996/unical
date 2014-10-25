ceb1ff51363ca1dd3cfea1f8b7c92573b1bd7fe2
Frederik Lassen <frederiklassen@gmail.com>
2014-10-25 15:33:32 +0200

    Makes shit work

--- api/controllers/LectioController.js
+++ api/controllers/LectioController.js
@@ -4,17 +4,40 @@
  * @description :: Server-side logic for managing lectios
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var ical = require('ical-generator');
 
 module.exports = {
 	timetable: function (req, res) {
+		var params = req.params;
 		// [ branch: '517', section: 'asdasd', year: '2014' ]
 		//return res.json();
-		return res.send("Test");
+		Lectio.find({school_id: params.branch, user_id: params.section, year: params.year}).exec(function findCB(error,found){
+			if (found.length == 0) {
+				return res.send("Nope :(");
+			} 
+
+			var cal = ical();
+
+			cal.setDomain('unical.co').setName('It\'s very awesome!');
+
+			found.forEach(function (event, index) {
+				cal.addEvent({
+					start: 			event.start_time,
+					end: 			event.end_time,
+					summary: 		event.text,
+					description: 	event.text,
+					location: 		event.location_text
+				});
+			});
+
+			return res.send(cal.toString());
+		});
 	},
 
-	colect: function (req, res) {
+	collect: function (req, res) {
+		var params = req.params;
 		// [ branch: '517', section: 'asdasd', year: '2014', week: '42' ]
-
+		LectioTimetable.get(params.branch, params.section, params.year, params.week);
 		return res.send("Collection");
 	}
 };
